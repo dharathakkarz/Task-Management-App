a9a8039bc82f16f325389caaa0342878412eeae6
dharathakkarz <160134908+dharathakkarz@users.noreply.github.com>
2024-02-27 10:43:31 +0530

    Update SignupForm.jsx

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -54,38 +54,35 @@ const SignupForm = () => {
             </div>
 
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-              <form>
-                <div className="flex flex-row items-center justify-center lg:justify-start">
-                  <p className="mb-0 mr-4 text-lg">Register Your Self</p>
-                
-                </div>
+<form className="p-8 max-w-md mx-auto bg-white rounded-xl shadow-md">
+  <div className="flex flex-row items-center justify-center lg:justify-start mb-6">
+    <p className="mb-0 mr-4 text-lg">Register Youe Self </p>
+  </div>
 
-                
+  <div className="mb-4">
+    <label htmlFor="name" className="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-1">Name</label>
+    <input type="text" name="name" id="name" value={formData.name} placeholder="Your name" onChange={handleChange} className="w-full px-3 py-2 placeholder-gray-500 border rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+    {fieldError("name")}
+  </div>
 
-                <div className="mb-4">
-                  <label htmlFor="name" className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-1">Name</label>
-                  <input type="text" name="name" id="name" value={formData.name} placeholder="Your name" onChange={handleChange} />
-                  {fieldError("name")}
-                </div>
+  <div className="mb-4">
+    <label htmlFor="email" className="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-1">Email</label>
+    <input type="text" name="email" id="email" value={formData.email} placeholder="youremail@domain.com" onChange={handleChange} className="w-full px-3 py-2 placeholder-gray-500 border rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+    {fieldError("email")}
+  </div>
 
-                <div className="mb-4">
-                  <label htmlFor="email" className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-1">Email</label>
-                  <input type="text" name="email" id="email" value={formData.email} placeholder="youremail@domain.com" onChange={handleChange} />
-                  {fieldError("email")}
-                </div>
+  <div className="mb-4">
+    <label htmlFor="password" className="after:content-['*'] after:ml-0.5 after:text-red-500 block mb-1">Password</label>
+    <input type="password" name="password" id="password" value={formData.password} placeholder="Your password.." onChange={handleChange} className="w-full px-3 py-2 placeholder-gray-500 border rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+    {fieldError("password")}
+  </div>
 
-                <div className="mb-4">
-                  <label htmlFor="password" className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-1">Password</label>
-                  <input type="password" name="password" id="password" value={formData.password} placeholder="Your password.." onChange={handleChange} />
-                  {fieldError("password")}
-                </div>
+  <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-sky-500/100' onClick={handleSubmit}>Submit</button>
 
-                <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-sky-500/100' onClick={handleSubmit}>Submit</button>
-
-                <div className='pt-4'>
-                  <Link to="/login" className='text-blue-400'>Already have an account? Login here</Link>
-                </div>
-              </form>
+  <div className='pt-4'>
+    <Link to="/login" className='text-blue-400'>Already have an account? Login here</Link>
+  </div>
+</form>
             </div>
           </div>
         </div>
